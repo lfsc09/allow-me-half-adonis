f6320a65d83a713d0fcb1d0edb567d6a8bb8dcab
Lucas Castro <lfsc09@gmail.com>
2025-03-11 22:48:17 -0300

    refactor(routes): added routes for login, home and logout

--- start/routes.ts
+++ start/routes.ts
@@ -6,7 +6,17 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+const UserController = () => import('#controllers/users_controller')
+
+router.get('/home', [UserController, 'homeRender'])
+
+router
+  .group(() => {
+    router.get('', [UserController, 'loginRender'])
+    router.post('', [UserController, 'login'])
+  })
+  .prefix('/login')
+
+router.get('/logout', [UserController, 'logout'])
