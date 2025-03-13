e2db9145c94cd460234f6a3352d6ebed1df1e92f
Lucas Castro <lfsc09@gmail.com>
2025-03-12 22:42:59 -0300

    refactor: added new methods for User crud and others

--- app/services/dao/user_database.ts
+++ app/services/dao/user_database.ts
@@ -1,6 +1,7 @@
-import { AccessToken } from '@adonisjs/auth/access_tokens'
-import { UserDAO } from '../../../contracts/user_dao.js'
 import User from '#models/user'
+import { AccessToken } from '@adonisjs/auth/access_tokens'
+import { Secret } from '@adonisjs/core/helpers'
+import { CreateUserInput, UserDAO } from '../../../contracts/user_dao.js'
 
 export class UserDAODatabase implements UserDAO {
   async getUserToken(email: string, password: string): Promise<AccessToken> {
@@ -8,12 +9,42 @@ export class UserDAODatabase implements UserDAO {
     return User.accessTokens.create(user)
   }
 
-  async getUser(id: string): Promise<User> {
-    return User.findOrFail(id)
+  async verifyUserToken(token: string): Promise<boolean> {
+    return User.accessTokens.verify(new Secret(token)) !== null
+  }
+
+  async getUser(userId: string): Promise<User> {
+    return User.findOrFail(userId)
+  }
+
+  async getUsers(): Promise<User[]> {
+    return (await User.all()).sort((a, b) => a.fullName.localeCompare(b.fullName))
+  }
+
+  async isUserAdmin(userId: string): Promise<boolean> {
+    const user = await User.findOrFail(userId)
+    return user.isAdmin
   }
 
   async deleteUserToken(userId: string, tokenId: string): Promise<void> {
     const user = await User.findOrFail(userId)
     await User.accessTokens.delete(user, tokenId)
+  }
+
+  async refreshUser(userId: string): Promise<void> {
+    const user = await User.findOrFail(userId)
+    user.rgm = User.newRgm()
+    user.cardCode = User.newCardCode()
+    user.validUntil = User.newValidUntil()
+    await user.save()
+  }
+
+  async createUser(input: CreateUserInput): Promise<User> {
+    return await User.create(input)
+  }
+
+  async deleteUser(userId: string): Promise<void> {
+    const user = await User.find(userId)
+    if (user) await user.delete()
   }
 }
--- contracts/user_dao.ts
+++ contracts/user_dao.ts
@@ -3,6 +3,21 @@ import { AccessToken } from '@adonisjs/auth/access_tokens'
 
 export abstract class UserDAO {
   abstract getUserToken(email: string, password: string): Promise<AccessToken>
-  abstract getUser(id: string): Promise<User>
+  abstract verifyUserToken(token: string): Promise<boolean>
+  abstract getUser(userId: string): Promise<User>
+  abstract getUsers(): Promise<User[]>
+  abstract isUserAdmin(userId: string): Promise<boolean>
   abstract deleteUserToken(userId: string, tokenId: string): Promise<void>
+  abstract refreshUser(userId: string): Promise<void>
+  abstract createUser(input: CreateUserInput): Promise<User>
+  abstract deleteUser(userId: string): Promise<void>
+}
+
+export type CreateUserInput = {
+  isAdmin?: boolean
+  avatarExt: string
+  email: string
+  password: string
+  fullName: string
+  course: string
 }
